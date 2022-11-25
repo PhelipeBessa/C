170b8105b88c0332df741c02a5c465b13242181b
Phelipe Bessa <phelipe.diogoti@gmail.com>
2022-11-25 18:15:22 -0300

    Create tabuada.c

---
+++ tabuada/tabuada.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch,
+system("pause") or input loop */
+int main() {
+int tabuada, x;
+printf("Digite a tabuda que voce deseja: ");
+scanf("%d", &tabuada);
+for (x = 1; x <= 10; ++x){
+printf("\n%d x %d = %d\n", tabuada, x, tabuada * x);
+}
+return 0;
+}
